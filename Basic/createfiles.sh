40d4a3fdeb07d8f3e197330bb50e74825b752bfc
Sai natha reddy Nooli <nooli.sainathareddy@gmail.com>
2026-05-11 11:34:01 -0500

    Adding files

--- Basic/createfiles.sh
+++ Basic/createfiles.sh
@@ -2,17 +2,13 @@
 
 cd Test
 
-#while IFS= read -r line; do
-#  echo "$line"
-#done <<<  "$(ls -lt)"
-newest_file=$(ls -lt | awk 'NR==2 {print $9}')
+touch log_$(date +'%y_%m_%d_%H_%M_%S').log
+sleep 10
+touch log_$(date +'%y_%m_%d_%H_%M_%S').log
+sleep 10
+touch log_$(date +'%y_%m_%d_%H_%M_%S').log
+sleep 10
+touch log_$(date +'%y_%m_%d_%H_%M_%S').log
+sleep 10
+touch log_$(date +'%y_%m_%d_%H_%M_%S').log
 
-echo "${newest_file}"
-
-cp ${newest_file}  ${newest_file}_$(date +'%y_%m_%d')
-
-oldest_file=$(ls -ltr | awk 'NR==2 {print $9}')
-
-echo "${oldest_file}"
-
-rm ${oldest_file}
